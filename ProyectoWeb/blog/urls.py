1036692bf1a6ec3f6b7c461539c1f32f836d3c79
zeuspod <josseluism@gmail.com>
2021-12-19 21:56:06 -0400

    second commit

--- ProyectoWeb/blog/urls.py
+++ ProyectoWeb/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 path('', views.blog, name='blog'),
+path('categoria/<int:categoria_id>/', views.categoria, name='categoria'),
 ]
--- ProyectoWeb/servicios/urls.py
+++ ProyectoWeb/servicios/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     
-    path('servicios', views.servicios, name='servicios'),
+    path('', views.servicios, name='servicios'),
     
 ]
 
