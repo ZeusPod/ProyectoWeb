5e75b9aa08a64f98b056bbb1996177f6d3c38539
zeuspod <josseluism@gmail.com>
2021-12-28 13:21:24 -0400

    add db tienda

--- ProyectoWeb/tienda/models.py
+++ ProyectoWeb/tienda/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class CategoriaProd(models.Model):
+    nombre = models.CharField(max_length=100)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "categoriaProd"
+        verbose_name_plural = "categoriasProd"
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+    imagen = models.ImageField(upload_to='tienda/static/img/', blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+    def __str__(self):
+        return self.nombre
