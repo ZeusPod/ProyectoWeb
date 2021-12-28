ad1e88db08116b292558e9e1b0ae6173fe6963e7
zeuspod <josseluism@gmail.com>
2021-12-28 09:13:34 -0400

    act send email

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -131,3 +131,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# config email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'latindevs@gmail.com'
+EMAIL_HOST_PASSWORD = 'zeus16102459'
+
--- ProyectoWeb/contacto/views.py
+++ ProyectoWeb/contacto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from .forms import FormularioContacto
 from django.contrib import messages
+from django.core.mail import EmailMessage, send_mail
+from django.conf import settings
 
 # Create your views here.
 def contacto(request):
@@ -13,9 +15,22 @@ def contacto(request):
             email = request.POST.get('email')
             mensaje = request.POST.get('mensaje')
             messages.success(request, 'Mensaje enviado correctamente')
-
-            return redirect('contacto/contacto/?valid')
-
+            #enviamos el correo con la informacion del formulario
+            
+            email = EmailMessage(
+                'Mensaje de contacto desde Rapid',
+                'el usario {} ha enviado el siguiente mensaje: {} y su correo electronico es {}'.format(nombre,mensaje,email),
+                '',
+                [settings.EMAIL_HOST_USER],
+                reply_to=[email],
+            )
+            
+            try:
+                email.send()
+                return redirect('/contacto/?valid')
+            except:
+                pass
+               # return redirect('/contacto/?invalid')
 
     return render (request , "contacto/contacto.html", {'miFormulario':formulario_contacto})
 
